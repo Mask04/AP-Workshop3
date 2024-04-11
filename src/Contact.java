7c10b96c1cfabece6790a0bd4c1f9f0d63f2869a
NandaMask <nandamask4@gmail.com>
2024-04-11 15:59:11 +0330

    PhoneBook

--- src/Contact.java
+++ src/Contact.java
@@ -3,14 +3,48 @@ public class Contact {
     private String email;
     private String firstName;
     private String lastName;
-    private String phoneNumber;
+    private PhoneNumber phoneNumber;
     private Address address;
-    public Contact(String group , String email , String firstName , String lastName , String phoneNumber , Address address){
+    public Contact(String group , String email , String firstName , String lastName , String countryCode , String number,
+                   String zipCode , String country , String city){
         this.group = group;
         this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.address  = address;
+        this.phoneNumber = new PhoneNumber(countryCode , number);
+        this.address  = new Address(zipCode ,country , city);
+    }
+
+    public void print()
+    {
+        System.out.println("group:" +group +'\n');
+        System.out.println("email:" +email + '\n');
+        System.out.println("firstName:" +firstName +'\n');
+        System.out.println("lastName:" + lastName + '\n');
+        System.out.println("phoneNumber:" + phoneNumber + '\n');
+        System.out.println("address:" + address +'\n');
+        System.out.println("***********************\n");
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGroup() {
+        return group;
     }
 }
---
+++ src/PhoneBook.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class PhoneBook {
+    private ArrayList <Contact> contacts;
+    public PhoneBook(ArrayList<Contact> contacts){
+        this.contacts = new ArrayList<>();
+    }
+    public boolean addContact(Contact newContact){
+        for ( Contact contact: contacts){
+            if (contact.equals(newContact)){
+                return false;
+            }
+        }
+        contacts.add(newContact);
+        return true;
+    }
+
+    public boolean deleteContact(String firstName , String lastName){
+        Iterator <Contact> it = contacts.iterator();
+        while (it.hasNext()){
+            Contact contact = it.next();
+            if (contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName)){
+                it.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Contact findContact(String firstName , String lastName){
+        for (Contact contact : contacts){
+            if (contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName))
+                return contact;
+        }
+        return null;
+    }
+
+    public ArrayList<Contact> findContacts(String group){
+        ArrayList<Contact> sameGroup = new ArrayList<>();
+        for (Contact contact : contacts){
+            if (contact.getGroup().equals(group)) {
+                sameGroup.add(contact);
+                return sameGroup;
+            }
+        }
+        return null;
+    }
+
+    public void printContacts(){
+        for (Contact contact : contacts) {
+            contact.print();
+        }
+
+    }
+}
