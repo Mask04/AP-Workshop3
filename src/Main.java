db1ec7d3fbbe693c238d3db76b54afb52a5ab109
NandaMask <nandamask4@gmail.com>
2024-04-18 18:10:43 +0330

    switch case version

--- src/Main.java
+++ src/Main.java
@@ -16,59 +16,62 @@ public class Main {
         PhoneBook myPhoneBook = new PhoneBook(contacts);
         while(!str.equals("exit")){
             String [] str2 = str.split(" ");
-            if (str2[0].equals("contacts-a")){
-                System.out.println("please enter contact's group");
-                group = scan.nextLine();
-                System.out.println("please enter contact's email");
-                email = scan.nextLine();
-                System.out.println("please enter contact's countryCode");
-                countryCode = scan.nextLine();
-                System.out.println("please enter contact's phoneNumber");
-                phoneNumber = scan.nextLine();
-                while(phoneNumber.length() != 10){
-                    System.out.println("invalid number");
+            switch (str2[0]) {
+                case "contacts-a": {
+                    System.out.println("please enter contact's group");
+                    group = scan.nextLine();
+                    System.out.println("please enter contact's email");
+                    email = scan.nextLine();
+                    System.out.println("please enter contact's countryCode");
+                    countryCode = scan.nextLine();
                     System.out.println("please enter contact's phoneNumber");
                     phoneNumber = scan.nextLine();
-                }
-                System.out.println("please enter contact's zipCode");
-                zipCode = scan.nextLine();
-                System.out.println("please enter contact's country");
-                country = scan.nextLine();
-                System.out.println("please enter contact's city");
-                city = scan.nextLine();
-                Contact contact = new Contact(group , email, str2[1] , str2[2], countryCode, phoneNumber
-                        , zipCode , country , city);
+                    while (phoneNumber.length() != 10) {
+                        System.out.println("invalid number");
+                        System.out.println("please enter contact's phoneNumber");
+                        phoneNumber = scan.nextLine();
+                    }
+                    System.out.println("please enter contact's zipCode");
+                    zipCode = scan.nextLine();
+                    System.out.println("please enter contact's country");
+                    country = scan.nextLine();
+                    System.out.println("please enter contact's city");
+                    city = scan.nextLine();
+                    Contact contact = new Contact(group, email, str2[1], str2[2], countryCode, phoneNumber
+                            , zipCode, country, city);
 
-                if(myPhoneBook.addContact(contact))
-                    System.out.println("success");
-                else
-                    System.out.println("duplicate-id");
-            }
-            else if (str2[0].equals("contacts-r")){
-                if (myPhoneBook.deleteContact(str2[1] , str2[2]))
-                    System.out.println("success");
-                else
-                    System.out.println("not-found");
-            }
-            else if (str2[0].equals("show-g")){
-                ArrayList <Contact> sameGroup = myPhoneBook.findContacts(str2[1]);
-                if (sameGroup.size() == 0){
-                    System.out.println("not-found");
+                    if (myPhoneBook.addContact(contact))
+                        System.out.println("success");
+                    else
+                        System.out.println("duplicate-id");
+                    break;
                 }
-                for (Contact contact : sameGroup) {
-                    contact.print();
+                case "contacts-r":
+                    if (myPhoneBook.deleteContact(str2[1], str2[2]))
+                        System.out.println("success");
+                    else
+                        System.out.println("not-found");
+                    break;
+                case "show-g":
+                    ArrayList<Contact> sameGroup = myPhoneBook.findContacts(str2[1]);
+                    if (sameGroup.size() == 0) {
+                        System.out.println("not-found");
+                    }
+                    for (Contact contact : sameGroup) {
+                        contact.print();
+                    }
+                    break;
+                case "show-c": {
+                    Contact contact = myPhoneBook.findContact(str2[1], str2[2]);
+                    if (contact == null) {
+                        System.out.println("not-found");
+                    } else
+                        contact.print();
+                    break;
                 }
-            }
-            else if (str2[0].equals("show-c")){
-                Contact contact = myPhoneBook.findContact(str2[1] , str2[2]);
-                if (contact == null) {
-                    System.out.println("not-found");
-                }
-                else
-                    contact.print();
-            }
-            else if (str2[0].equals("show")){
-                myPhoneBook.printContacts();
+                case "show":
+                    myPhoneBook.printContacts();
+                    break;
             }
             str = scan.nextLine();
         }
